de5ffcfd89f76d53b353e71cf732094c73f7e9b5
Kamyk1 <kamil.krukowicz@op.pl>
2026-01-25 01:13:58 +0100

    ARCH

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+START_DIR="$(pwd)"
+
 cp config/wallpapers/mywallpaper1.mp4 ~/Wideo/
 
-echo "🔧 Installing packages..."
+echo "Installing packages..."
 while read -r pkg; do
     if pacman -Qi "$pkg" &>/dev/null; then
         echo "$pkg already installed"
@@ -13,15 +15,19 @@ while read -r pkg; do
     fi
 done < packages.txt
 
-echo "📁 Setting up PipeWire config..."
+clear
+
+echo "Setting up PipeWire config..."
 mkdir -p "$HOME/.config/pipewire/pipewire.conf.d"
 cp -r config/pipewire/pipewire.conf.d/* "$HOME/.config/pipewire/pipewire.conf.d/"
 
-echo "🔁 Restarting PipeWire services..."
+echo "Restarting PipeWire services..."
 systemctl --user enable --now pipewire pipewire-pulse wireplumber
 systemctl --user restart pipewire pipewire-pulse wireplumber
 
-echo "🔧 Configuring UFW firewall..."
+clear
+
+echo "Configuring UFW firewall..."
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
@@ -34,8 +40,29 @@ sudo ufw enable
 
 sudo ufw status verbose
 
-echo "✅ Installation complete!"
+clear
 
+echo "Setting up BetterDiscord"
+curl -fsSL https://bun.com/install | bash
+
+cd ~/
+git clone --single-branch -b main https://github.com/BetterDiscord/BetterDiscord.git
+cd ~/BetterDiscord
+bun install
+bun run build
+bun inject
+
+echo "Setting up Kde Themes"
+
+sh "$START_DIR/install-kde.sh"
+
+echo "Setting up Alacritty"
+
+git clone https://github.com/rose-pine/alacritty.git
+cp ./alacritty/dist/* ~/.config/alacritty
+printf '\n[general]\nimport = ["rose-pine.toml"]\n' > ~/.config/alacritty/alacritty.toml
+
+echo "Installation complete!"
 rm -rf $HOME/Pulpit/dotfiles
 
 sleep 5
